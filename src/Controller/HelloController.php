2ee450cb5b33888ef0545ef239af1c22601caff4
ursus164 <Galawieprzowa@gmail.com>
2024-12-13 00:19:36 +0100

    Added Hello controller with two actions for respective routes in URL

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+    // type declaration for safety
+    private array $messages = [
+        'Hello',
+        'Hi',
+        'Bye'
+    ];
+
+    #[Route('/', name: 'app_index')]
+    public function index() : Response
+    {
+        return new Response(implode(',', $this -> messages));
+    }
+
+    #[Route('messages/{id}', name:'app_show_one')]
+    public function showOne($id) : Response 
+    {
+        return new Response($this -> messages[$id]);
+       
+    }
+}
