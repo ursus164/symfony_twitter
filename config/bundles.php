92181a7a8b1c68cbaacbaba959ab139dcc00ecb4
ursus164 <Galawieprzowa@gmail.com>
2024-12-13 13:33:17 +0100

    Added database and entities with maker bundle - also connection parameters were added in symfony config

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
