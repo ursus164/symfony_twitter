f394ee40f88be4c7776c4fd6a9a93b901efc761e
ursus164 <Galawieprzowa@gmail.com>
2024-12-13 19:57:18 +0100

    Form html twig and MicroPost Controller

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use DateTime;
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -51,36 +52,33 @@ class MicroPostController extends AbstractController
         ]);
     }
 
-    #[Route('/micro-post/add', name: 'app_micro_post_add', priority:2)]
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
     public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
         $microPost = new MicroPost();
 
         // microPost object will be filled with data provided by a form - that's why we pass it to builder
 
-        $form = $this->createFormBuilder($microPost)
-            ->add('title')
-            ->add('text')
-            ->add('submit', SubmitType::class,['label' => 'save'])
-            ->getForm();
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
 
-            // forms by default are being sent ti the same path they were rendered in - so to handle the form submission we will use the same action that renders the form
 
-            $form -> handleRequest($request); // handleRequest method will get the data that is being sent in request and try to match them with fields that this form defines including any validation constraints by default
+        // forms by default are being sent ti the same path they were rendered in - so to handle the form submission we will use the same action that renders the form
 
-            if($form -> isSubmitted() && $form -> isValid()) {
-                $post = $form -> getData();
-                $post -> setCreatedAt(new DateTime());
-                $entityManager -> persist($post);
-                $entityManager -> flush();
+        $form->handleRequest($request); // handleRequest method will get the data that is being sent in request and try to match them with fields that this form defines including any validation constraints by default
 
-                // Add response flash message
-                $this -> addFlash('success', 'Your post was added!');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreatedAt(new DateTime());
+            $entityManager->persist($post);
+            $entityManager->flush();
 
-                // Redirect to different page
-                return $this -> redirectToRoute('app_micro_post');
-            }
-            // dd($post);
+            // Add response flash message
+            $this->addFlash('success', 'Your post was added!');
+
+            // Redirect to different page
+            return $this->redirectToRoute('app_micro_post');
+        }
+        // dd($post);
 
         return $this->render(
             'micro_post/add.html.twig',
@@ -92,27 +90,23 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
     public function edit(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response
     {
-    
-        $form = $this->createFormBuilder($post)
-            ->add('title')
-            ->add('text')
-            ->add('submit', SubmitType::class,['label' => 'Save'])
-            ->getForm();
 
-            $form -> handleRequest($request); 
+        $form = $this->createForm(MicroPostType::class, $post);
 
-            if($form -> isSubmitted() && $form -> isValid()) {
-                $post = $form -> getData();
-                $entityManager -> persist($post);
-                $entityManager -> flush();
+        $form->handleRequest($request); // inspects request and calls submit if submitted
 
-                // Add response flash message
-                $this -> addFlash('success', 'Post was updated!');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $entityManager->persist($post);
+            $entityManager->flush();
 
-                // Redirect to different page
-                return $this -> redirectToRoute('app_micro_post');
-            }
-            // dd($post);
+            // Add response flash message
+            $this->addFlash('success', 'Post was updated!');
+
+            // Redirect to different page
+            return $this->redirectToRoute('app_micro_post');
+        }
+        // dd($post);
 
         return $this->render(
             'micro_post/edit.html.twig',
