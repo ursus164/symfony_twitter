774694ef2874aeabf9a5374ed982e2f9bbc55ab7
ursus164 <Galawieprzowa@gmail.com>
2024-12-14 10:20:11 +0100

    Added playground for One To One relationship betwen User and UserProfile - basic records were added to DB and removed from it

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,17 +19,34 @@ class HelloController extends AbstractController
         ['value' => 'Hye', 'created' => '2022/02/12']
     ];
 
-    #[Route('/{limit<\d+>?3}', name: 'app_index')] // optional param with default value of
-    public function index(int $limit): Response
+    #[Route('/', name: 'app_index')] // optional param with default value of
+    public function index(EntityManagerInterface $entityManager, UserProfileRepository $profiles): Response
     {
-        return $this->render(
+        // $user = new User();
+        // $user->setEmail('test@example.com');
+        // $user->setPassword('123456');
+        // user is not directly added to DB, instead, when saving profile it will be added (cascading)
+        
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+
+        // $entityManager->persist($profile);
+        // $entityManager->flush();
+
+        // Find user profile by user ID
+        // $userProfile = $profiles->findOneBy(['user' => 8]);
+
+        // if ($userProfile) {
+        //     $entityManager -> remove($userProfile);
+        //     $entityManager -> flush();
+        // }
+
+        
+
+        return $this -> render(
             'hello/index.html.twig',
-            [
-                'messages' => $this->messages,
-                'limit' => $limit
-            ]
+            ['messages' => $this -> messages]
         );
-        // return new Response(implode(',', array_slice($this -> messages,0,$limit))); // joins elements of arrray
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
