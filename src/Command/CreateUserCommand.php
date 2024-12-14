bbf6b935c01a96489ca282f52c4939bc64dda202
ursus164 <Galawieprzowa@gmail.com>
2024-12-14 13:09:57 +0100

    Added custom symfony console command for creating users

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Creates a new user account',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+    private UserPasswordHasherInterface $userPasswordHasher,
+    private EntityManagerInterface $entityManager
+) {
+    parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'User email')
+            ->addArgument('password', InputArgument::REQUIRED, 'User password')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $password
+            )
+        );
+
+        $this -> entityManager -> persist($user);
+
+        // if ($arg1) {
+        //     $io->note(sprintf('You passed an argument: %s', $arg1));
+        // }
+
+        // if ($input->getOption('option1')) {
+        //     // ...
+        // }
+
+        $io->success(sprintf('User %s was created succesfully!',$email));
+
+        return Command::SUCCESS;
+    }
+}
