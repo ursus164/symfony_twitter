aa43ab928eaf487c175de02508b0b379400945b0
ursus164 <Galawieprzowa@gmail.com>
2025-01-08 18:30:41 +0100

    Added login feature to the app

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(
+        AuthenticationUtils $utils
+    ): Response
+    {
+        $lastUsername = $utils -> getLastUsername();
+        $authError = $utils -> getLastAuthenticationError();
+
+        return $this->render('login/index.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $authError
+        ]);
+    }
+}
